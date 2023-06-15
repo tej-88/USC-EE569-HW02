6041fe55951e6ad2cdd222bf22cb21b2ef83b919
Tejas <tejas8acharya@gmail.com>
2023-06-15 09:16:00 +0530

    Add Image data structure

--- fixed_thresholding/fixed.h
+++ fixed_thresholding/fixed.h
@@ -1,3 +1,27 @@
+#ifndef FIXED
+#define FIXED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define MAX_INTENSITY 255
+#define MIN_INTENSITY 0
+
+typedef enum {rgb = 3, gray = 1} colors;
+
+typedef unsigned char byte;
+
+typedef struct Image
+{
+    int rows;
+    int cols;
+    colors color;
+    byte ***image;
+}Image;
 
 
 Image *fixed_thresholding(Image *image_ptr, byte threshold);
+
+#endif
